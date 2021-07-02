815520d52fd73b830c726c558c0f0ae71840e1b0
Bukola Omojowo <bomojowo@gmail.com>
2021-07-02 14:44:50 +0100

    imported hasASymbol file to add conditionally rendered feedback for symbols

--- src/components/WordDescription.tsx
+++ src/components/WordDescription.tsx
@@ -1,4 +1,5 @@
 import { hasAVowel } from "../utils/hasAVowel"
+import {hasASymbol} from "../utils/hasASymbol"
 
 interface WordDescriptionProps {
   wordToDescribe: string;
@@ -8,12 +9,14 @@ function WordDescription({ wordToDescribe }: WordDescriptionProps): JSX.Element 
   const hasAtLeastFiveCharacters = wordToDescribe.length > 5;
   const containsAVowel = hasAVowel(wordToDescribe);
   const hasTheLetterQ = wordToDescribe.toLowerCase().match('q');
+  const containsASymbol = hasASymbol(wordToDescribe);
 
   return (
     <ul>
       {hasAtLeastFiveCharacters && <li>Has at least five characters</li>}
       {containsAVowel && <li>Contains a vowel</li>}
       {hasTheLetterQ && <li>Contains the letter Q</li>}
+      {containsASymbol && <li>Contains a symbol</li>}
     </ul>
   )
 }
