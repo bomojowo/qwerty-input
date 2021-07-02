4b78b0008f5007e83d48e9479b2e0b857c50c7d1
Bukola Omojowo <bomojowo@gmail.com>
2021-07-02 14:59:00 +0100

    Added another conditionally rendered feeback

--- src/components/WordDescription.tsx
+++ src/components/WordDescription.tsx
@@ -1,15 +1,20 @@
-import { hasAVowel } from "../utils/hasAVowel"
-import {hasASymbol} from "../utils/hasASymbol"
+import { hasAVowel } from "../utils/hasAVowel";
+import { hasASymbol } from "../utils/hasASymbol";
 
 interface WordDescriptionProps {
   wordToDescribe: string;
 }
 
-function WordDescription({ wordToDescribe }: WordDescriptionProps): JSX.Element {
+function WordDescription({
+  wordToDescribe,
+}: WordDescriptionProps): JSX.Element {
   const hasAtLeastFiveCharacters = wordToDescribe.length > 5;
   const containsAVowel = hasAVowel(wordToDescribe);
-  const hasTheLetterQ = wordToDescribe.toLowerCase().match('q');
+  const hasTheLetterQ = wordToDescribe.toLowerCase().match("q");
   const containsASymbol = hasASymbol(wordToDescribe);
+  const hasAtLeastSevenChar = wordToDescribe.length > 7;
+  const hasAtLeastOneLowerCase = wordToDescribe.toLocaleLowerCase;
+  const hasAtLeastOneUpperCase = wordToDescribe.toLocaleLowerCase;
 
   return (
     <ul>
@@ -17,8 +22,12 @@ function WordDescription({ wordToDescribe }: WordDescriptionProps): JSX.Element 
       {containsAVowel && <li>Contains a vowel</li>}
       {hasTheLetterQ && <li>Contains the letter Q</li>}
       {containsASymbol && <li>Contains a symbol</li>}
+      {containsASymbol &&
+        hasAtLeastSevenChar &&
+        hasAtLeastOneLowerCase &&
+        hasAtLeastOneUpperCase && <li>Impressively strong Input</li>}
     </ul>
-  )
+  );
 }
 
-export default WordDescription
+export default WordDescription;
