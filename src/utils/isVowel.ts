2dd611e861f27320699e2a10be12c3b74e40a7ad
Bukola Omojowo <bomojowo@gmail.com>
2021-07-05 13:37:11 +0100

    Added comment

--- src/utils/isVowel.ts
+++ src/utils/isVowel.ts
@@ -1,4 +1,4 @@
 export function isVowel(character: string): boolean {
   const vowels = ["a", "e", "i", "o", "u"];
-  return vowels.includes(character.toLowerCase());
+  return vowels.includes(character.toLowerCase()); //returns true if charactor is vowel
 }
