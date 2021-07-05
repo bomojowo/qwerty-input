fd8a6abf825d512738116ce2e4236355d872fdbe
Bukola Omojowo <bomojowo@gmail.com>
2021-07-05 15:15:56 +0100

    Added an onChange event handler

--- src/components/UncontrolledInput.tsx
+++ src/components/UncontrolledInput.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 function UncontrolledInput(): JSX.Element {
+  const [typedMessage, setTypedMessage] = useState('')
   return (
     <>
       <p>Here's an input:</p>
-      <input />
+      <input 
+      value={typedMessage}
+      onChange={(e) =>{
+        setTypedMessage(e.target.value)
+      }}/>
+      <button
+        onClick={() => console.log({typedMessage})}
+      >
+        Log input
+      </button>
       <p>
         But it's hard to detect what it contains (in a nice React-friendly way)
       </p>
       <p>So I can't tell you anything about it...</p>
+      <p>{typedMessage}</p>
     </>
   );
 }
