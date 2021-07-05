89745af39ae8d752eeb6c4559400180ac1ae4065
Bukola Omojowo <bomojowo@gmail.com>
2021-07-05 13:36:38 +0100

    imported function and used to conditional render

--- src/components/WordDescription.tsx
+++ src/components/WordDescription.tsx
@@ -1,5 +1,6 @@
 import { hasAVowel } from "../utils/hasAVowel";
-import { hasASymbol } from "../utils/hasASymbol";
+//import { hasASymbol } from "../utils/hasASymbol";
+import { isStrongInput} from "../utils/isStrongInput"
 
 interface WordDescriptionProps {
   wordToDescribe: string;
@@ -8,26 +9,32 @@ interface WordDescriptionProps {
 function WordDescription({
   wordToDescribe,
 }: WordDescriptionProps): JSX.Element {
-  const hasAtLeastFiveCharacters = wordToDescribe.length > 5;
+  // const hasAtLeastFiveCharacters = wordToDescribe.length > 5;
   const containsAVowel = hasAVowel(wordToDescribe);
+  const impressivelyStrong = isStrongInput(wordToDescribe)
   const hasTheLetterQ = wordToDescribe.toLowerCase().match("q");
-  const containsASymbol = hasASymbol(wordToDescribe);
-  const hasAtLeastSevenChar = wordToDescribe.length > 7;
-  const hasAtLeastOneLowerCase = wordToDescribe.toLocaleLowerCase;
-  const hasAtLeastOneUpperCase = wordToDescribe.toLocaleLowerCase;
+  // const containsASymbol = hasASymbol(wordToDescribe);
+  // const hasAtLeastSevenChar = wordToDescribe.length > 7;
+  // const hasAtLeastOneLowerCase = wordToDescribe.toLocaleLowerCase;
+  // const hasAtLeastOneUpperCase = wordToDescribe.toLocaleUpperCase;
+  console.log(impressivelyStrong)
 
   return (
     <ul>
-      {hasAtLeastFiveCharacters && <li>Has at least five characters</li>}
+      {/* {hasAtLeastFiveCharacters && <li>Has at least five characters</li>} */}
       {containsAVowel && <li>Contains a vowel</li>}
       {hasTheLetterQ && <li>Contains the letter Q</li>}
-      {containsASymbol && <li>Contains a symbol</li>}
+      {/*{containsASymbol && <li>Contains a symbol</li>}
       {containsASymbol &&
         hasAtLeastSevenChar &&
         hasAtLeastOneLowerCase &&
-        hasAtLeastOneUpperCase && <li>Impressively strong Input</li>}
+        hasAtLeastOneUpperCase && <li>Impressively strong Input</li>} */}
+        {impressivelyStrong && <li>Impressively Strong Input</li>}
+        
     </ul>
+    
   );
+  
 }
 
 export default WordDescription;
